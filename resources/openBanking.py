e28d82893398dfbd32fd5f591f2d06a0ae233796
jisu0427 <93955242+jisu0427@users.noreply.github.com>
2022-03-21 14:13:10 +0900

    test_commit

--- resources/openBanking.py
+++ resources/openBanking.py
@@ -9,6 +9,9 @@ from flask.json import jsonify
 from mysql_connection import get_connection
 from mysql.connector.errors import Error
 
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from dateutil.relativedelta import *
+
 
 #### 고정값
 # client_id
@@ -25,28 +28,39 @@ from mysql.connector.errors import Error
 # ?code=0&client_id=fde4d72d-e26b-492c-9d66-d7ef9014cd59&client_secret=980fb060-8387-4dd1-8f3a-989d3083fe4e&redirect_uri=http://localhost:5000/&grant_type=authorization_code
 
 class testResource(Resource) :
+    @jwt_required()
     def get(self) :
+        # jwt에서 발행된 토큰 이용
+        user_id = get_jwt_identity()
+        print('user_id = ',user_id)
+
         get_code = request.args.get('code')
 
         
-        params = {"code":get_code, "client_id": "fde4d72d-e26b-492c-9d66-d7ef9014cd59", "client_secret": "980fb060-8387-4dd1-8f3a-989d3083fe4e", "redirect_uri":"http://localhost:5000/","grant_type": "authorization_code"}
-        print(params)
+        params = {"code":get_code, "client_id": "fde4d72d-e26b-492c-9d66-d7ef9014cd59", "client_secret": "980fb060-8387-4dd1-8f3a-989d3083fe4e", "redirect_uri":"http://localhost:5000/","grant_type": "authorization_code", }
+        # print(params)
         info = requests.post(Config.Token_GET_URL, params=params)
         
         info = info.json()
         print(type(info))
+        print(info)
         
+        ## expires_in -> sec 단위로 온다.
+        ## expires_in -> relativedelta (second=)
+        expires_in = info['expires_in']
+        expires_date = expires_date+relativedelta(seconds=expires_in)
 
         try :
             # 1. DB 에 연결
             cnt = get_connection()
            
+           
             # 2. 쿼리문 
-            query = '''update into user SET
-                        access_token=%s, token_type=%s, refresh_token=%s, expires_in=%s, user_seq_no=%s
-                        where user_seq_no=%s;'''
+            query = '''update user SET access_token=%s,  refresh_token=%s, expires_date=%s, user_seq_no=%s where id=%s;'''
             print(query)
-            record = (info['access_token'], info['token_type'], info['refresh_token'], info['expires_in'], info['user_seq_no'] )
+            record = (info['access_token'],  info['refresh_token'], expires_date, info['user_seq_no'] , user_id )
+            
+            
             
 
             # 커넥션으로부터 커서를 가져온다.
@@ -54,6 +68,9 @@ class testResource(Resource) :
 
             # 쿼리문을 커서에 넣어서 실행한다.
             cursor.execute(query, record)
+
+
+
 
             # 커넥션 커밋.=> 디비에 영구적으로 반영
             cnt.commit()
@@ -72,5 +89,5 @@ class testResource(Resource) :
                 cnt.close()
                 print('MySQL connection is closed')
 
-        # access_token = info['access_token']
-        # return {'result': '완벽', 'access_token':access_token}
+        access_token = info['access_token']
+        return {'result': '완벽', 'access_token':access_token}
--- test.py
+++ test.py
@@ -5,15 +5,17 @@ from numpy import dtype
 import requests
 from config import *
 from flask.json import jsonify
-
+from dateutil.relativedelta import *
+import datetime as dt
 
 def test():
-    get_code = '4dlNRkdo1stR6PJm1Tt3D2ox2kNgQL'
-    params = {"code":get_code, "client_id": "fde4d72d-e26b-492c-9d66-d7ef9014cd59", "client_secret": "980fb060-8387-4dd1-8f3a-989d3083fe4e", "redirect_uri":"http://localhost:5000/","grant_type": "authorization_code"}
-    info = requests.post(Config.Token_GET_URL, params=params)
+    b = dt.date(2020, 7, 25)
+    # print(b)
+    # b= b.strftime('%Y%m%d')
+    # print(b)
 
-    info = info.text
-    print(info)
-
+    
+    a = b+relativedelta(days=50)
+    print(a)
 
 test()
