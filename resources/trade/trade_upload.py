3b66d0518fd141fcda6f48a0bce438f5b65910ce
jkong72 <96038721+jkong72@users.noreply.github.com>
2022-03-21 14:42:58 +0900

    계좌 및 거래정보 API 가작성

--- resources/trade/trade_upload.py
+++ resources/trade/trade_upload.py
@@ -1,72 +1,145 @@
+from typing import final
 from xml.etree.ElementInclude import include
 from flask_restful import Resource
 from flask import request
 from http import HTTPStatus
 from mysql.connector.errors import Error
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from datetime import datetime as dt
-from dateutil.relativedelta import relativedelta
-import requests
-
-from utils.url_path import url_binder
 
 
-# 등록 계좌 조회 (핀테크 얻기 위함)
-def get_account (user_seq_no):
-    base_url = 'https://testapi.openbanking.or.kr/v2.0/account/list?'
-    user_seq_no = 'user_seq_no={}'.format(user_seq_no)
-    include_cancel_yn = 'include_cancel_yn=N'
-    sort_order = 'sort_order=D'
+from mysql_connection import get_connection
+from utils.url_request import url_binder, get_account, get_trade
 
-    url_list = [base_url, user_seq_no, include_cancel_yn, sort_order]
-    url = url_binder(url_list) # url의 파라미터를 엮어서 url을 완성하는 함수
 
-    account_result = requests.get(url)
-    return account_result
+# 계좌 정보 DB 통신문
+class AccountInfoResource(Resource):
+    # DB에서 계좌 정보 가져오기
+    @jwt_required() # 헤더를 통해 토큰을 받음
+    def get(self):
+        try: # 통신문
+            connection = get_connection() # DB와 연결
+            user_id = get_jwt_identity    # 이용자 식별 (user_id)
+            query = '''select tran_date, tran_time, branch_name, inout_type, tran_amt, account_id, type_id, memo
+            from account
+            where user_id = %s'''
 
-# 등록 계좌에서 핀테크 가져오는 부분 (분리해야함)
-result = get_account()
-result['res_cnt']                   #계좌 개수
-for account in result['res_list']:
-    account['account_alias']        #계좌 이름
-    account['account_num_masked']   #계좌 번호 (일부 가려짐)
-    account['account_holder_name']  #계좌주 이름
-    account['bank_name']            #은행 이름
-    account['fintech_use_num']      #핀테크 넘버
-    account['account_type']         #계좌 유형 (1:수시입출금, 2:예적금, 6:수익증권, T:종합계좌)
+            param = (user_id,)
+            cursor = connection.cursor()
+            cursor.execute(query, param)
+            record_list = cursor.fetchall()
 
-# 거래 내역 조회
-def get_trade (fintech_num):
-    today_date = dt.today() # 현재 날짜
-    back_date = today_date - relativedelta(years=2)
-    today_date = today_date.strftime('%y%m%d')
-    back_date = back_date.strftime('%y%m%d')
+        except Error as err: # 예외처리 (에러)
+            return {'Error':str(err)}
+        finally:
+            cursor.close()
+            
+        return {'data':'0'}
 
-    base_url = 'https://testapi.openbanking.or.kr/v2.0/account/transaction_list/fin_num?' # 기본 URL
-    inquiry_base = 'inquiry_base=D&'                                   # 조회 기준 (D:일간, T: 시간)
-    sort_order = 'sort_order=D&'                                       # 정렬 순서
-    inquiry_type = 'inquiry_type=A&'                                   # 조회 구분 (A: 모두, I: 입금, O: 출금)
-    bank_tran_id = 'bank_tran_id=M202200391U{}'.format()               # 은행 거래 고유번호(매일 순차 증가)
-    fintech_use_num = 'fintech_use_num={}'.format(fintech_num)         # 핀테크 번호
-    from_date = 'from_date={}'.format(today_date)                      # 조회시작일.
-    to_date = 'to_date={}'.format(back_date)                           # 조회종료일
-    befor_inquiry_trace_info = 'befor_inquiry_trace_info={}'.format()  # 페이지(0~19)
-    tran_dtime = 'tran_dtime={}'.format(today_date)                    # 요청 일시 (오늘 날짜)
+    # DB에 계좌 정보 쓰기 (오픈뱅킹에서 가져오기)
+    @jwt_required() # 헤더를 통해 토큰을 받음
+    def post(self, user_seq_no):
+        try: # 통신문
+            connection = get_connection() # DB와 연결
+            user_id = get_jwt_identity() # 이용자 식별 (user_id)
 
-    url_list = [base_url, inquiry_type, inquiry_base, sort_order, bank_tran_id, fintech_use_num, from_date, to_date, befor_inquiry_trace_info, tran_dtime]
-    url = url_binder(url_list)
+            result = get_account(user_seq_no) # 등록계좌조회
 
-    trade_result = requests.get(url)
-    return trade_result
+            for account in result['res_list']: # 계좌 정보 가져오기
+                account_alias = account['account_alias']              #계좌 이름
+                account_num_masked = account['account_num_masked']    #계좌 번호 (일부 가려짐)
+                account_holder_name = account['account_holder_name']  #계좌주 이름
+                bank_name = account['bank_name']                      #은행 이름
+                fintech_use_num = account['fintech_use_num']          #핀테크 넘버
+                account_type = account['account_type']                #계좌 유형 (1:수시입출금, 2:예적금, 6:수익증권, T:종합계좌)
 
-# 거래 내역 조회에서 정보 가져오는 부분 (분리해야함)
-repeat = 'y'             # 값이 실행될 수 있게 설정
-while repeat == 'y':     # 다음 페이지가 있다면 반복
-    result = get_trade()
-    repeat = result['next_page_yn'] # 다음 페이지 여부
-    for trade in result['res_list']:
-        trade['tran_date']          # 거래일
-        trade['tran_time']          # 거래시
-        trade['inout_type']         # 입출금
-        trade['print_content']      # 통장인자
-        trade['tran_amt']           # 거래액
+                # query문
+                # account 테이블에
+                # 현재 유저id, 계좌 이름, 계좌 번호, 계좌주 이름, 은행 이름, 핀테크 번호, 계좌 유형을 저장
+                query = '''insert into account
+                (user_id, account_alias, account_num_masked, account_holder_name, bank_name, fintech_num, account_type)
+                values
+                (%s, %s, %s, %s, %s, %s, %s); '''
+
+                param = (user_id, account_alias, account_num_masked, account_holder_name, bank_name, fintech_use_num)
+
+                # 커서 연결 및 실행
+                cursor = connection.cursor()    # 커서 가져오기
+                cursor.execute(query, param)    # 커서 실행
+                connection.commit()             # 반영
+
+        except Error as err: # 예외처리
+            return {'Error': str(err)}
+        
+        # 커서 및 연결 종료
+        finally :
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
+
+# 거래 내역 DB 통신문
+class TradeInfoResource(Resource):
+    # DB에서 거래 내역 가져오기
+    @jwt_required() # 헤더를 통해 토큰을 받음
+    def get(self):
+        try: # 통신문
+            connection = get_connection() # DB와 연결
+            user_id = get_jwt_identity    # 이용자 식별 (user_id)
+            query = '''select
+                            tran_date, tran_time, print_content, inout_type, tran_amt, account_id, type_id, memo
+                        from trade
+                        where user_id = %s
+                        order by tran_date, tran_time desc'''
+
+            param = (user_id,)
+            cursor = connection.cursor()
+            cursor.execute(query, param)
+            record_list = cursor.fetchall()
+
+        except Error as err: # 예외처리 (에러)
+            return {'Error':str(err)}
+        finally:
+            cursor.close()
+            
+        return {'data':'0'}
+
+    # DB에 거래 내역 쓰기 (오픈뱅킹에서 가져오기)
+    @jwt_required() # 헤더를 통해 토큰을 받음
+    def post(self):
+        try: # 통신문
+            connection = get_connection() # DB와 연결
+            user_id = get_jwt_identity() # 이용자 식별 (user_id)
+
+            repeat == 'y'                       #실행을 위한 초기 값
+            while repeat == 'y':                #다음 페이지가 있을 때만 실행
+                result = get_trade()            # 등록계좌조회
+                repeat = result['next_page_yn'] # 다음 페이지가 있는지 여부
+
+                for trade in result['res_list']: # 거래정보 가져오기
+                    tran_date = trade['tran_date']          # 거래일
+                    tran_time = trade['tran_time']          # 거래시
+                    inout_type = trade['inout_type']        # 입출금
+                    print_content = trade['print_content']  # 통장인자
+                    tran_amt = trade['tran_amt']            # 거래액
+
+                    # query문
+                    query = '''insert into trade
+                    (user_id, trand_date, tran_time, inout_type, print_content, tran_amt)
+                    values
+                    (%s, %s, %s, %s, %s, %s)'''
+
+                    param = (user_id, tran_date, tran_time, inout_type, print_content, tran_amt)
+
+                    # 커서 연결 및 실행
+                    cursor = connection.cursor()    # 커서 가져오기
+                    cursor.execute(query, param)    # 커서 실행
+                    connection.commit()               # 반영
+
+        except Error as err: # 예외처리
+            return {'Error': str(err)}
+
+        # 커서 및 연결 종료
+        finally :
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
---
+++ utils/url_request.py
@@ -0,0 +1,53 @@
+import requests
+import datetime as dt
+from dateutil.relativedelta import relativedelta
+
+# url의 파라미터를 엮어서 url을 완성하는 함수
+def url_binder (url_list):
+    url = ''
+    for word in url_list:
+        url = url+word
+        if url_list.index(word) != 0: # 첫번째가 아니라면 &로 이어줌
+            url = url+'&'
+
+    url = url.strip('&') # 맨 마지막에 붙은 & 제거
+    return url
+
+
+# 등록 계좌 조회 (핀테크 얻기 위함)
+def get_account (user_seq_no):
+    base_url = 'https://testapi.openbanking.or.kr/v2.0/account/list?'
+    user_seq_no = 'user_seq_no={}'.format(user_seq_no)
+    include_cancel_yn = 'include_cancel_yn=N'
+    sort_order = 'sort_order=D'
+
+    url_list = [base_url, user_seq_no, include_cancel_yn, sort_order]
+    url = url_binder(url_list) # url의 파라미터를 엮어서 url을 완성하는 함수
+
+    account_result = requests.get(url)
+    return account_result
+
+# 거래 내역 조회
+def get_trade (fintech_num):
+    today_date = dt.today() # 현재 날짜
+    back_date = today_date - relativedelta(years=2)
+    today_date = today_date.strftime('%y%m%d')
+    back_date = back_date.strftime('%y%m%d')
+
+    base_url = 'https://testapi.openbanking.or.kr/v2.0/account/transaction_list/fin_num?' # 기본 URL
+    inquiry_base = 'inquiry_base=D&'                                   # 조회 기준 (D:일간, T: 시간)
+    sort_order = 'sort_order=D&'                                       # 정렬 순서
+    inquiry_type = 'inquiry_type=A&'                                   # 조회 구분 (A: 모두, I: 입금, O: 출금)
+    bank_tran_id = 'bank_tran_id=M202200391U{}'.format()               # 은행 거래 고유번호(매일 순차 증가)
+    fintech_use_num = 'fintech_use_num={}'.format(fintech_num)         # 핀테크 번호
+    from_date = 'from_date={}'.format(today_date)                      # 조회시작일.
+    to_date = 'to_date={}'.format(back_date)                           # 조회종료일
+    befor_inquiry_trace_info = 'befor_inquiry_trace_info={}'.format()  # 페이지(0~19)
+    tran_dtime = 'tran_dtime={}'.format(today_date)                    # 요청 일시 (오늘 날짜)
+
+    url_list = [base_url, inquiry_type, inquiry_base, sort_order, bank_tran_id, fintech_use_num, from_date, to_date, befor_inquiry_trace_info, tran_dtime]
+    url = url_binder(url_list)
+
+    trade_result = requests.get(url)
+    return trade_result
+
