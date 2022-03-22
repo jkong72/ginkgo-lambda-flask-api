333da8ae97a69d3fca74bcd331a2f6a313a06c9f
jkong72 <96038721+jkong72@users.noreply.github.com>
2022-03-22 15:19:57 +0900

    utils 파일 구조 정리

--- utils/openbaking_req.py
+++ utils/openbaking_req.py
@@ -30,22 +30,22 @@ def get_account (user_seq_no):
     return account_result
 
 # 거래 내역 조회
-def get_trade (fintech_num):
+def get_trade (fintech_num, page):
     today_date = dt.today() # 현재 날짜
     back_date = today_date - relativedelta(years=2)
     today_date = today_date.strftime('%y%m%d')
     back_date = back_date.strftime('%y%m%d')
 
     base_url = 'https://testapi.openbanking.or.kr/v2.0/account/transaction_list/fin_num?' # 기본 URL
-    inquiry_base = 'inquiry_base=D&'                                   # 조회 기준 (D:일간, T: 시간)
-    sort_order = 'sort_order=D&'                                       # 정렬 순서
-    inquiry_type = 'inquiry_type=A&'                                   # 조회 구분 (A: 모두, I: 입금, O: 출금)
-    bank_tran_id = 'bank_tran_id=M202200391U{}'.format()               # 은행 거래 고유번호(매일 순차 증가)
-    fintech_use_num = 'fintech_use_num={}'.format(fintech_num)         # 핀테크 번호
-    from_date = 'from_date={}'.format(today_date)                      # 조회시작일.
-    to_date = 'to_date={}'.format(back_date)                           # 조회종료일
-    befor_inquiry_trace_info = 'befor_inquiry_trace_info={}'.format()  # 페이지(0~19)
-    tran_dtime = 'tran_dtime={}'.format(today_date)                    # 요청 일시 (오늘 날짜)
+    inquiry_base = 'inquiry_base=D&'                               # 조회 기준 (D:일간, T: 시간)
+    sort_order = 'sort_order=D&'                                   # 정렬 순서
+    inquiry_type = 'inquiry_type=A&'                               # 조회 구분 (A: 모두, I: 입금, O: 출금)
+    bank_tran_id = 'bank_tran_id=M202200391U{}'.format()           # 은행 거래 고유번호(매일 순차 증가)
+    fintech_use_num = 'fintech_use_num={}'.format(fintech_num)     # 핀테크 번호
+    from_date = 'from_date={}'.format(today_date)                  # 조회시작일
+    to_date = 'to_date={}'.format(back_date)                       # 조회종료일
+    befor_inquiry_trace_info = 'befor_inquiry_trace_info={}'.format(page)  # 페이지(0~19)
+    tran_dtime = 'tran_dtime={}'.format(today_date)                # 요청 일시 (오늘 날짜)
 
     url_list = [base_url, inquiry_type, inquiry_base, sort_order, bank_tran_id, fintech_use_num, from_date, to_date, befor_inquiry_trace_info, tran_dtime]
     url = url_binder(url_list)
