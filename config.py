bc09b45e3043f201b6cc7e2c1c3471aaa8a748ac
jkong72 <96038721+jkong72@users.noreply.github.com>
2022-03-24 10:29:48 +0900

    로컬 경로 추가

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 class Config :
-    JWT_SECRET_KEY = 'yh@1234'
+    LOCAL_URL = 'http://127.0.0.1:5000'
     END_POINT = 'https://n5dbtf6emg.execute-api.us-east-1.amazonaws.com/dev'
+    JWT_SECRET_KEY = 'yh@1234'
