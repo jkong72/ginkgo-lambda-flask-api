38abccede28acfa08c123450810e27f083557d06
jisu0427 <93955242+jisu0427@users.noreply.github.com>
2022-03-28 17:04:28 +0900

    Update openBanking.py

--- resources/openBanking.py
+++ resources/openBanking.py
@@ -11,41 +11,28 @@ from mysql.connector.errors import Error
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from dateutil.relativedelta import *
 
-
-#### 고정값
-# client_id
-# redirect_uri
-# scope
-# state
-# auth_type
-
-#### code 값을 가져온다
-# response_type
-
-
-
-# ?code=0&client_id=fde4d72d-e26b-492c-9d66-d7ef9014cd59&client_secret=980fb060-8387-4dd1-8f3a-989d3083fe4e&redirect_uri=http://localhost:5000/&grant_type=authorization_code
-
 class OpenBankingResource(Resource) :
-    @jwt_required()
+    # @jwt_required() # 이용자 인증 (jwt) 주석 해제 #todo 
     def post(self) :
-        # jwt에서 발행된 토큰 이용
-        user_id = get_jwt_identity()
-        print('user_id = ',user_id)
+        user_email = request.get_json()["data"]
+        print (user_email)
+        # user_id = get_jwt_identity() # 이용자 식별 (jwt 토큰)
+        # print('user_id = ',user_id)
 
+        # 파라미터 지정
         get_code = request.args.get('code')
         client_id = "fde4d72d-e26b-492c-9d66-d7ef9014cd59"
         client_secret = "980fb060-8387-4dd1-8f3a-989d3083fe4e"
-        redirect_uri = "http://localhost:8501/"
+        redirect_uri = "http://localhost:8501"
         grant_type = "authorization_code"
 
-        
+        # 오픈뱅킹 액세스 토큰 발급
         params = {"code":get_code, "client_id": client_id, "client_secret": client_secret, "redirect_uri":redirect_uri,"grant_type": grant_type, }
-        # print(params)
-        info = requests.post(Config.Token_GET_URL, params=params)
-        
+        info = requests.post(Config.Token_GET_URL)
         info = info.json()
-        print(type(info))
+
+
+        print ('#'*3 + 'print_info' + '#'*10)
         print(info)
         
         ## expires_in -> sec 단위로 온다.
@@ -56,18 +43,32 @@ class OpenBankingResource(Resource) :
         try :
             # 1. DB 에 연결
             connection = get_connection()
-           
-           
+
+            query = '''select id from user where email = %s;
+            '''
+
+            param = (user_email,)
+            cursor = connection.cursor(dictionary = True)
+            cursor.execute(query, param)
+            record_list = cursor.fetchall()
+
+            user_id = record_list['id']
+
             # 2. 쿼리문 
             query = '''update user SET access_token=%s,  refresh_token=%s, expires_date=%s, user_seq_no=%s where id=%s;'''
             print(query)
             record = (info['access_token'],  info['refresh_token'], expires_date, info['user_seq_no'] , user_id )
+            
+            
+            
 
             # 커넥션으로부터 커서를 가져온다.
             cursor = connection.cursor()
 
             # 쿼리문을 커서에 넣어서 실행한다.
             cursor.execute(query, record)
+
+
 
 
             # 커넥션 커밋.=> 디비에 영구적으로 반영
@@ -87,7 +88,5 @@ class OpenBankingResource(Resource) :
                 connection.close()
                 print('MySQL connection is closed')
 
-        # access_token = info['access_token']
-        # print(access_token)
-        print('openbanking')
-        return {'result': 0, 'access_token':info['access_token']}
+        access_token = info['access_token']
+        return {'result': 0, 'access_token':access_token}
