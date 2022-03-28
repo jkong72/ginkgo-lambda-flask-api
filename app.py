71f1b839de86d1b387e6e86540dcb220c4340e97
jkong72 <96038721+jkong72@users.noreply.github.com>
2022-03-28 11:38:32 +0900

    plotly 설치

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, render_template
 from flask.json import jsonify
 from flask_restful import Api
 from http import HTTPStatus
@@ -36,5 +36,10 @@ api.add_resource(TradeInfoResource, '/trade')                       # DB에서 �
 api.add_resource(BankTranIdResource, '/bank_tran_id')                       # 은행 거래 코드 입출
 
 
+# HTML
+@app.route('/')
+def chart_tester():
+    return render_template('chart.html')
+
 if __name__ == '__main__' :
-    app.run()
+    app.run(debug=True)
